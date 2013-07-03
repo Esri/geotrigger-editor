f6a55e49202ebedbb8f8a909d04646eaa48bebe9
Nate Goldman <nnmgoldman@gmail.com>
2013-07-03 14:11:19 -0700

    dependencies

--- src/geotrigger-editor.js
+++ src/geotrigger-editor.js
@@ -1,1 +1,56 @@
-console.log('Hello Loqi');
+(function(){
+
+  function dependencyTest() {
+    var deps = [
+      [JSON, 'JSON'],
+      [$,'jQuery'],
+      [_,'underscore'],
+      [Backbone,'Backbone'],
+      [Backbone.Marionette,'Backbone.Marionette'],
+      [Geoservices,'Geoservices'],
+      [Geotriggers,'Geotriggers'],
+      [L,'Leaflet'],
+      [L.esri,'Esri-leaflet']
+    ];
+
+    console.log('Roll Call:');
+
+    for (var i=0; i<deps.length; i++) {
+      console.log(typeof deps[i][0] !== 'undefined' ? '√' : 'x', deps[i][1]);
+    }
+  }
+
+  function mapTest() {
+    var map = L.map('map');
+
+    // ArcGIS Online Basemaps - Streets, Topographic, Gray, Gray Labels, Ocean, NationalGeographic, Imagery, ImageryLabels
+    L.esri.basemapLayer("Streets").addTo(map);
+
+    function onLocationFound(e) {
+      var radius = e.accuracy / 2;
+      L.marker(e.latlng).addTo(map).bindPopup("You are within " + radius + " meters from this point").openPopup();
+      L.circle(e.latlng, radius).addTo(map);
+    }
+
+    function onLocationError(e) {
+      alert(e.message);
+    }
+
+    map.on('locationfound', onLocationFound);
+    map.on('locationerror', onLocationError);
+
+    map.locate({setView: true, maxZoom: 16});
+  }
+
+  function init() {
+    dependencyTest();
+    mapTest();
+  }
+
+  if (typeof window === 'object') {
+    window.onload = init;
+  } else {
+    console.log('hello console');
+  }
+
+})();
