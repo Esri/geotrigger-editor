45d2250b211acae34b604e04f94de61896b6e2a2
Paul C Pederson <ppederson@esri.com>
2013-07-03 15:48:05 -0700

    adding grunt watch and compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,38 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'dist/<%= pkg.name %>.min.js'
       }
+    },
+    watch: {
+      scripts: {
+        files: ['src/*.js','src/scss/*'],
+        tasks: ['compass'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    compass: {
+      dist: {                   // Production
+        options: {
+          sassDir: 'src/scss',
+          cssDir: 'src/css',
+          environment: 'production'
+        }
+      },
+      dev: {                    // Another target
+        options: {
+          sassDir: 'src/scss',
+          cssDir: 'src/css'
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint','uglify']);
