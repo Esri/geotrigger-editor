ed7820ed77951bcf36d23a7ba752aa00830d2475
Paul C Pederson <ppederson@esri.com>
2013-07-23 14:48:57 -0700

    new draw module

---
+++ src/js/GTEdit.Draw.js
@@ -0,0 +1,12 @@
+GTEdit.module('Draw', function(Draw, App, Backbone, Marionette, $, _) {
+
+  Draw = {
+    polygon: function(e) {
+      console.log("give me the polygon tool for a second");
+    },
+    radius: function(e) {
+      console.log("give me the radius tool for a second");
+    }
+  };
+
+});
