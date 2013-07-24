6479cab491f25283593bf16c12053adbaec73504
Nikolas Wise <nikolaswise@gmail.com>
2013-07-24 15:18:23 -0700

    tooltips hidden by default, appear when mouse over map

---
+++ src/js/overrides.js
@@ -0,0 +1,7 @@
+L.Tooltip.prototype.updatePosition = function(latlng) {
+  var pos = this._map.latLngToLayerPoint(latlng);
+  L.DomUtil.setPosition(this._container, pos);
+  this._container.style.display = 'inline-block';
+  console.log('doing something');
+  return this;
+};
