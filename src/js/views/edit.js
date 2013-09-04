4c752a8332feae8b985be8b7e59191612abb5c31
Nate Goldman <nnmgoldman@gmail.com>
2013-09-03 18:19:40 -0700

    add update logic

--- src/js/views/edit.js
+++ src/js/views/edit.js
@@ -9,6 +9,10 @@ GeotriggerEditor.module('Views', function(Views, App, Backbone, Marionette, $, _
     template: 'edit',
     className: 'gt-edit',
 
+    events: {
+      'click .gt-submit': 'parseForm'
+    },
+
     onShow: function() {
       var item = this.options.item;
       var layer;
@@ -25,6 +29,52 @@ GeotriggerEditor.module('Views', function(Views, App, Backbone, Marionette, $, _
 
     restoreShape: function() {
       this.options.item.restoreShape();
+    },
+
+    parseForm: function(e) {
+      e.preventDefault();
+      var data = this.$el.find('form').serializeObject();
+
+      if (data) {
+        this.updateTrigger(data);
+        App.vent.trigger('controls:list:toggle');
+      }
+    },
+
+    updateTrigger: function(data) {
+      console.log(data);
+      var geo;
+      var layer = App.Map.Draw.editLayer.getLayers()[0];
+
+      if (layer instanceof L.Circle) {
+        var latlng = layer.getLatLng();
+        geo = {
+          'latitude': latlng.lat,
+          'longitude': latlng.lng,
+          'distance': layer.getRadius()
+        };
+      } else {
+        geo = {
+          'geojson': layer.toGeoJSON()
+        };
+      }
+
+      var trigger = {
+        'triggerId': this.model.get('triggerId'),
+        'condition': {
+          'direction': 'enter',
+          'geo': geo
+        },
+        'action': {
+          'notification': {
+            'text': 'Welcome to Portland'
+          },
+          'callbackUrl': 'http://pdx.gov/welcome'
+        },
+        'setTags': ['newtags']
+      };
+
+      App.vent.trigger('trigger:update', trigger);
     }
   });
 
