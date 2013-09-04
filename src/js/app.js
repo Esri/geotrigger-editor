c84edab1a84c5be0f0388040db199016d7211c4a
Nate Goldman <nnmgoldman@gmail.com>
2013-09-03 22:54:00 -0700

    add main view, app initializer, clean up vendor

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,19 @@
 
 var GeotriggerEditor = new Backbone.Marionette.Application();
 
-GeotriggerEditor.addRegions({
-  controlsRegion: '#gt-controls-region',
-  listDrawerRegion: '.gt-drawer-list',
-  newDrawerRegion: '.gt-drawer-new',
-  mapRegion: '#gt-map-region',
-  notificationsRegion: '#gt-notifications'
+GeotriggerEditor.addInitializer(function(options) {
+  var el = options && options.el ? options.el : '#gt-editor';
+  var mainView = new this.Views.Main();
+
+  this.addRegions({ mainRegion: el });
+  this.mainRegion.show(mainView);
+  this.addRegions({
+    controlsRegion: '#gt-controls-region',
+    listDrawerRegion: '#gt-drawer-list',
+    newDrawerRegion: '#gt-drawer-new',
+    mapRegion: '#gt-map-region',
+    notificationsRegion: '#gt-notifications'
+  });
 });
 
 GeotriggerEditor.on('initialize:after', function() {
