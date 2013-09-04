1662461fb1db86c5a04e753cdf172b22b5cc47a2
Nate Goldman <nnmgoldman@gmail.com>
2013-09-04 10:25:14 -0700

    proper object serialization

--- src/js/lib/jquery.serializeObject.js
+++ src/js/lib/jquery.serializeObject.js
@@ -1,40 +1,69 @@
-//
-// Use internal $.serializeArray to get list of form elements which is
-// consistent with $.serialize
-//
-// From version 2.0.0, $.serializeObject will stop converting [name] values
-// to camelCase format. This is *consistent* with other serialize methods:
-//
-//   - $.serialize
-//   - $.serializeArray
-//
-// If you require camel casing, you can either download version 1.0.4 or map
-// them yourself.
-//
+// https://gist.github.com/ngoldman/dbe69b48cd21bf0d9a47
 
 (function($){
-  $.fn.serializeObject = function () {
-    "use strict";
+  $.fn.serializeObject = function(options){
 
-    var result = {};
-    var extend = function (i, element) {
-      var node = result[element.name];
+    options = $.extend({}, options);
 
-  // If node with same name exists already, need to convert it to an array as it
-  // is a multi-value field (i.e., checkboxes)
+    var self = this,
+      json = {},
+      push_counters = {},
+      patterns = {
+        "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
+        "key":      /[a-zA-Z0-9_]+|(?=\[\])/g,
+        "push":     /^$/,
+        "fixed":    /^\d+$/,
+        "named":    /^[a-zA-Z0-9_]+$/
+      };
 
-      if ('undefined' !== typeof node && node !== null) {
-        if ($.isArray(node)) {
-          node.push(element.value);
-        } else {
-          result[element.name] = [node, element.value];
-        }
-      } else {
-        result[element.name] = element.value;
-      }
+    this.build = function(base, key, value){
+      base[key] = value;
+      return base;
     };
 
-    $.each(this.serializeArray(), extend);
-    return result;
+    this.push_counter = function(key){
+      if(push_counters[key] === undefined){
+        push_counters[key] = 0;
+      }
+      return push_counters[key]++;
+    };
+
+    $.each($(this).serializeArray(), function(){
+
+      // skip invalid keys
+      if(!patterns.validate.test(this.name)){
+        return;
+      }
+
+      var k,
+        keys = this.name.match(patterns.key),
+        merge = this.value,
+        reverse_key = this.name;
+
+      while((k = keys.pop()) !== undefined){
+
+        // adjust reverse_key
+        reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
+
+        // push
+        if(k.match(patterns.push)){
+          merge = self.build([], self.push_counter(reverse_key), merge);
+        }
+
+        // fixed
+        else if(k.match(patterns.fixed)){
+          merge = self.build([], k, merge);
+        }
+
+        // named
+        else if(k.match(patterns.named)){
+          merge = self.build({}, k, merge);
+        }
+      }
+
+      json = $.extend(true, json, merge);
+    });
+
+    return json;
   };
 })(jQuery);
