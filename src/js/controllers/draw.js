72de693eb075924d6e458f898aaa0e3b0c8932ed
Nate Goldman <nnmgoldman@gmail.com>
2013-09-11 11:32:23 -0700

    working config defaults and options

--- src/js/controllers/draw.js
+++ src/js/controllers/draw.js
@@ -14,8 +14,8 @@ GeotriggerEditor.module('Map.Draw', function(Draw, App, Backbone, Marionette, $,
 
     _setup: function() {
       // Initialize new Draw Handlers
-      this._tools.polygon = new L.Draw.Polygon(App.map, App.Config.editOptions);
-      this._tools.radius = new L.Draw.Circle(App.map, App.Config.editOptions);
+      this._tools.polygon = new L.Draw.Polygon(App.map, App.config.editOptions);
+      this._tools.radius = new L.Draw.Circle(App.map, App.config.editOptions);
 
       this._eventBindings();
     },
@@ -66,9 +66,9 @@ GeotriggerEditor.module('Map.Draw', function(Draw, App, Backbone, Marionette, $,
       var shape;
 
       if (geo.geojson) {
-        shape = App.Map.polygon(geo.geojson, App.Config.editOptions.shapeOptions, false).getLayers()[0];
+        shape = App.Map.polygon(geo.geojson, App.config.editOptions.shapeOptions, false).getLayers()[0];
       } else {
-        shape = App.Map.circle(geo, App.Config.editOptions.shapeOptions, false);
+        shape = App.Map.circle(geo, App.config.editOptions.shapeOptions, false);
       }
 
       return shape;
