7d917c648ead7018afc1f1d0ea1ef0689efae11c
Nate Goldman <nnmgoldman@gmail.com>
2013-10-29 10:44:06 -0700

    ie fixes

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -1,29 +1,24 @@
 GeotriggerEditor.module('API', function(API, App, Backbone, Marionette, $, _) {
 
   API.addInitializer(function(options){
-    try {
-      if (!options ||
-          !options.credentials ||
-          !options.credentials.clientId ||
-          !options.credentials.clientSecret) {
-        throw new Error('GeotriggerEditor requires a `credentials` object with `clientId` and `clientSecret` properties');
-      }
-
-      var sessionOptions = {
-        clientId: options.credentials.clientId,
-        clientSecret: options.credentials.clientSecret,
-        persistSession: false
-      };
-
-      if (options.proxy) {
-        sessionOptions.proxy = options.proxy;
-      }
-
-      this.session = new Geotriggers.Session(sessionOptions);
-
-    } catch (e) {
-      console.error(e.name + ": " + e.message);
+    if (!options ||
+        !options.credentials ||
+        !options.credentials.clientId ||
+        !options.credentials.clientSecret) {
+      throw new Error('GeotriggerEditor requires a `credentials` object with `clientId` and `clientSecret` properties');
     }
+
+    var sessionOptions = {
+      clientId: options.credentials.clientId,
+      clientSecret: options.credentials.clientSecret,
+      persistSession: false
+    };
+
+    if (options.proxy) {
+      sessionOptions.proxy = options.proxy;
+    }
+
+    this.session = new Geotriggers.Session(sessionOptions);
   });
 
 });
