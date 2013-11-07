8a9f575778deb4f1a342e2bc5952d344819315b3
Nate Goldman <nnmgoldman@gmail.com>
2013-11-07 10:59:09 -0800

    enforce config, clean up api

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 var GeotriggerEditor = new Backbone.Marionette.Application();
 
 GeotriggerEditor.addInitializer(function(options) {
-  var el = options && options.el ? options.el : '#gt-editor';
-  var layout = this.regions = new this.Layouts.Main();
-
-  this.addRegions({ mainRegion: el });
-  this.mainRegion.show(layout);
+  this.Config.start(options);
+  this.API.start();
+  this.regions = new this.Layouts.Main();
+  this.addRegions({ mainRegion: this.config.el });
+  this.mainRegion.show(this.regions);
 });
